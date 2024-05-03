f1b57c27ee7fea9f72d59cef8521ea5c1c0ddd7c
kthitirat <kthitirat@aru.ac.th>
2024-05-03 10:55:04 +0700

    first commit

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\Role;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        User::factory()->create([
+            'role_id' => Role::where('name', 'admin')->first()->id,
+            'name' => 'Kthitirat',
+            'email' => 'kthitirat@gmail.com'
+        ]);
+//        User::factory()->create([
+//            'role_id' => Role::where('name', 'manager')->first()->id,
+//            'name' => 'manager',
+//            'email' => 'manager@manager.com'
+//        ]);
+//        User::factory()->create([
+//            'role_id' => Role::where('name', 'user')->first()->id,
+//            'name' => 'user',
+//            'email' => 'user@user.com'
+//        ]);
+//        User::factory()->count(10)->create();
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Foundation\Application;
+use Illuminate\Support\Facades\Route;
+use Inertia\Inertia;
+use App\Http\Controllers\PageController;
+use App\Http\Controllers\AnnouncementController;
+use Laravel\Fortify\Features;
+use App\Http\Controllers\SubjectController;
+
+//Route::get('/login', [PageController::class, 'login'])
+//    ->name('login');
+//Route::post('/login', [PageController::class, 'doLogin']);
+if (Features::enabled(Features::registration())) {
+    Route::get('/register', [PageController::class, 'userRegister'])
+        ->name('register');
+    Route::post('/register', [PageController::class, 'storeRegister']);
+}
+
+
+Route::get('/', [PageController::class, 'index'])->name('index');
+Route::resource('/subjects', SubjectController::class);
+//Route::get('/announcements', [AnnouncementController::class, 'index'])->name('announcements.index');
+//Route::get('/get-all-announcements-type', [AnnouncementController::class, 'getAllAnnouncementTypes'])->name(
+//    'announcements.get_all_announcement_types'
+//);
+//Route::get('/get-all-announcements-categories', [AnnouncementController::class, 'getAllAnnouncementCategories'])->name(
+//    'announcements.get_all_announcement_categories'
+//);
+
+
+
